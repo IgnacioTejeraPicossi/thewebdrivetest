bd594750e32002a2d4b6f56868ff9a7be791620d
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2023-10-24 14:24:45 +0200

    TakeScreenshot test

--- src/test/java/keys/KeysTests.java
+++ src/test/java/keys/KeysTests.java
@@ -6,20 +6,22 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class KeyTests extends BaseTests {
+public class KeysTests extends BaseTests {
 
     @Test
     public void testBackspace(){
         var keyPage = homePage.clickKeyPresses();
         keyPage.enterText("A" + Keys.BACK_SPACE);
-        assertEquals(keyPage.getResult(),"You entered: BACK_SPACE" );
+        assertEquals(keyPage.getResult(), "You entered: BACK_SPACE", "Input result incorrect");
     }
+
     @Test
     public void testPi(){
         var keyPage = homePage.clickKeyPresses();
         keyPage.enterPi();
-        //assertEquals(keyPage.getResult(),"You entered: 6" );
+        /*
+            NOTE: we didn't finish this test in the video.
+            We debugged to watch it enter the pi key
+         */
     }
-
-
 }
