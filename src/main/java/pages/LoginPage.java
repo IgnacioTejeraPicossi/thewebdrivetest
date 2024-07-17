920c5792faa2cb1600a9a0beb3262644a8dccf02
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-07-17 14:21:32 +0200

    Fix LoginPage

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -10,13 +10,16 @@ public class LoginPage {
     private By loginButton = By.cssSelector("#login button");
 
     public LoginPage(WebDriver driver){
+
         this.driver = driver;
     }
 
-    public void setUsername(String username){
+    public void setUsername(String username)
+    {
         driver.findElement(usernameField).sendKeys(username);
     }
     public void setPassword(String password){
+
         driver.findElement(passwordField).sendKeys(password);
     }
     public SecureAreaPage clickLoginButton(){
